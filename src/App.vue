5c63f1d61555e403da98cf7fcedf9dd502c8b7df
chandrabhan357 <chandrabhan1366@hotmail.com>
2023-04-08 00:58:23 +0530

    Router Added

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
-  <template>
-    <div id="app">
-
-      <todo-app></todo-app>
-
-    </div>
-
+<template>
+  <nav>
+    <router-link to="/"></router-link>   
+    <router-link to="/about"></router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import TodoApp from './components/TodoApp.vue'
-
-export default {
-  name: 'App',
-  components: {
-    TodoApp
-  }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
 
+nav {
+  padding: 30px;
+}
 
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
